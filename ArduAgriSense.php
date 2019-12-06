0147cf33ce503dfd9db327e7455b450787ad5757
Miguel Tapia <mtapiafdez@gmail.com>
2019-12-05 22:36:54 -0500

    Changes

--- ArduAgriSense.php
+++ ArduAgriSense.php
@@ -1,9 +1,15 @@
 <?php
-	$time = time();
-	$tempF = $_POST["tempC"];
-	$file = 'dashboard.html';
-	$data = $time."  -  ".$tempF;
-	file_put_contents($file, $data);
+	// $time = time();
+	// $tempF = $_POST["tempC"];
+	// $file = 'dashboard.html';
+	// $data = $time."  -  ".$tempF;
+	// file_put_contents($file, $data);
+	
+	$tempC = $_POST["tempC"];
+	$dashboardContents = file_get_contents("dashboard.html");
+	$new_dashboard = str_replace("{{tempC}}", $tempC, $dashboardContents);
+	
+	file_put_contents("dashboard.html", $new_dashboard);
 	
 	echo "Data Sent!";
 ?>
